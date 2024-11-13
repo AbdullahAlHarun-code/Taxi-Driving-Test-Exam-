ea6c26d33608a7d4aa58785915696d3033c694ec
abdullahcodeacademy <abdullahcodeacademy@gmail.com>
2024-11-13 12:08:37 +0000

    create javascript file and note from algoexpert frontend lesson

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// sdfsdf
+/* 
+Number: 25, 10.42
+BigInt: 10n
+Boolean: true, false
+String: 'Hello', "Hello"
+Symbol: Symbol('symbol')
+Null: null (null must be assigned and it has no value)
+Undefined: undefined(the value is not assigned/set yet and don't initialize a variable with undefined and it will be undefined by default) 
+
+*/
+let a = 1;
+console.log(a);
